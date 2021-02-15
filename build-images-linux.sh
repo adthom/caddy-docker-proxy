c4f7cb92a87da25ae833795df4f6f2dabdcd2b99
Lucas Lara <lucaslorentzlara@hotmail.com>
2021-02-15 08:23:28 +0100

    Fix docker images platform

--- build-images-linux.sh
+++ build-images-linux.sh
@@ -3,9 +3,9 @@
 set -e
 
 chmod +x artifacts/binaries/linux/amd64/caddy
-docker build -t lucaslorentz/caddy-docker-proxy:ci -f Dockerfile .
-docker build -t lucaslorentz/caddy-docker-proxy:ci-alpine -f Dockerfile-alpine .
+docker build --platform linux/amd64 -t lucaslorentz/caddy-docker-proxy:ci -f Dockerfile .
+docker build --platform linux/amd64 -t lucaslorentz/caddy-docker-proxy:ci-alpine -f Dockerfile-alpine .
 
 chmod +x artifacts/binaries/linux/arm32v6/caddy
-docker build -t lucaslorentz/caddy-docker-proxy:ci-arm32v6 -f Dockerfile-arm32v6 .
-docker build -t lucaslorentz/caddy-docker-proxy:ci-alpine-arm32v6 -f Dockerfile-alpine-arm32v6 .
+docker build --platform linux/arm -t lucaslorentz/caddy-docker-proxy:ci-arm32v6 -f Dockerfile-arm32v6 .
+docker build --platform linux/arm -t lucaslorentz/caddy-docker-proxy:ci-alpine-arm32v6 -f Dockerfile-alpine-arm32v6 .
--- build-images-windows.sh
+++ build-images-windows.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker build -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-1803 -f Dockerfile-nanoserver-1803 .
-docker build -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-1809 -f Dockerfile-nanoserver-1809 .
+docker build --platform windows/amd64 -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-1803 -f Dockerfile-nanoserver-1803 .
+docker build --platform windows/amd64 -t lucaslorentz/caddy-docker-proxy:ci-nanoserver-1809 -f Dockerfile-nanoserver-1809 .
